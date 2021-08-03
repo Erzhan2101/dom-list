18631ab38f69481ecddbb6a0e931f379e198a3eb
Erzhan2101 <erzhan210198@gmail.com>
2021-08-03 17:25:41 +0600

    gg

--- script.js
+++ script.js
@@ -15,20 +15,23 @@ addBtn.addEventListener("click", () => {
     }
 })
 
+list.addEventListener("click", (event) => {
+    if (event.target.classList[0] === "cls-btn") {
+        event.target.parentNode.remove()
+    }
+    if (event.target.classList[0] === 'done'){
+        event.target.parentNode.classList.toggle('line-through')
+    }
+})
+
 textInput.addEventListener("keypress", (event) => {
     if (event.key === "Enter" && textInput.value.trim()) {
         list.innerHTML += `<li>
-          <input  class="cross-out"  type="checkbox">
+          <input  class="done"  type="checkbox">
                            ${textInput.value}
                            <button class="cls-btn">Удалить</button>
                            </li>`
         textInput.value = ""
-    }
-})
-
-list.addEventListener("click", (event) => {
-    if (event.target.classList[0] === "cls-btn") {
-        event.target.parentNode.remove()
     }
 })
 
