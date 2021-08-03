eeb0dd3c5cf552348c7b1612944f83079abf0c70
Erzhan2101 <erzhan210198@gmail.com>
2021-08-03 16:42:28 +0600

    ol-number

--- script.js
+++ script.js
@@ -6,10 +6,21 @@ const list = document.querySelector(".list")
 addBtn.addEventListener("click", () => {
     if (textInput.value.trim()) {
         list.innerHTML += `<li>
-                           <input type="checkbox">        
+                           <input type="checkbox" class="checkbox-1">        
                            ${textInput.value}
                            <button class="cls-btn" 
                             >Удалить</button>
+                           </li>`
+        textInput.value = ""
+    }
+})
+
+textInput.addEventListener("keypress", (event) => {
+    if (event.key === "Enter" && textInput.value.trim()) {
+        list.innerHTML += `<li>
+          <input  class="cross-out"  type="checkbox">
+                           ${textInput.value}
+                           <button class="cls-btn">Удалить</button>
                            </li>`
         textInput.value = ""
     }
@@ -20,4 +31,10 @@ list.addEventListener("click", (event) => {
         event.target.parentNode.remove()
     }
 })
+
+clearAll.addEventListener("click", () => {
+    list.innerHTML = ""
+})
+
+
 
