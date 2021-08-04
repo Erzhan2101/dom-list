3b7446d51f7fcbd313fcb9e4d5b05a3aa31244e5
Erzhan2101 <erzhan210198@gmail.com>
2021-08-04 15:43:42 +0600

    checkbox

--- script.js
+++ script.js
@@ -3,10 +3,11 @@ const addBtn = document.querySelector(".add-btn")
 const clearAll = document.querySelector(".clear-all")
 const list = document.querySelector(".list")
 
+
 addBtn.addEventListener("click", () => {
     if (textInput.value.trim()) {
         list.innerHTML += `<li>
-                           <input type="checkbox" class="checkbox-1">        
+                           <input type="checkbox" class="done">        
                            ${textInput.value}
                            <button class="cls-btn" 
                             >Удалить</button>
@@ -19,8 +20,14 @@ list.addEventListener("click", (event) => {
     if (event.target.classList[0] === "cls-btn") {
         event.target.parentNode.remove()
     }
-    if (event.target.classList[0] === 'done'){
-        event.target.parentNode.classList.toggle('line-through')
+    if(event.target.classList[0] === "done"){
+        if(event.target.checked){
+            event.target.parentNode.classList.add("line-through")
+            event.target.setAttribute("checked", "checked")
+        }else {
+            event.target.parentNode.classList.remove("line-through")
+            event.target.removeAttribute("checked")
+        }
     }
 })
 
@@ -39,5 +46,7 @@ clearAll.addEventListener("click", () => {
     list.innerHTML = ""
 })
 
-
+// if (event.target.classList[0] === 'done'){
+//     event.target.parentNode.classList.toggle('line-through')
+// }
 
